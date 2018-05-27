86f96519ed243109bce3ff9e59a9978d961ab564
Cristiano Santos <cristianossd@gmail.com>
2018-05-27 19:59:29 -0300

    Team CRUD resolvers

--- src/index.js
+++ src/index.js
@@ -1,38 +1,61 @@
 const { GraphQLServer } = require('graphql-yoga');
+const _ = require('lodash');
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Team!]!
-  }
+let teams = [{
+  id: 'team-0',
+  name: 'Maral Team',
+  category: 'RX',
+  members: ['Cristiano'],
+}];
 
-  type Team {
-    id: ID!
-    name: String!
-    category: String!
-    members: [String!]!
-    events: [Event]
-    finalScore: Int
-  }
-
-  type Event {
-    id: ID!
-    order: Int!
-    time: String
-    reps: Int
-    weight: Int
-    ranking: Int
-  }
-`;
+let idCount = teams.length;
 
 const resolvers = {
   Query: {
-    // missing resolvers
-  }
+    info: () => `This is the API of custom crossfit leaderboard`,
+    feed: () => teams,
+    team: (root, args) => {
+      return _.find(teams, (t) => t.name === args.name)
+    },
+  },
+
+  Mutation: {
+    newTeam: (root, args) => {
+      const team = {
+        id: `team-${idCount++}`,
+        name: args.name,
+        category: args.category,
+        members: args.members,
+      };
+
+      teams.push(team);
+      return team;
+    },
+
+    updateTeam: (root, args) => {
+      const index = _.findIndex(teams, (t) => t.name === args.name);
+      teams[index].category = args.category || teams[index].category;
+      teams[index].finalScore = args.finalScore || teams[index].finalScore;
+
+      return teams[index];
+    },
+
+    deleteTeam: (root, { name }) => {
+      const deleted = _.remove(teams, (t) => t.name === name);
+      return _.head(deleted);
+    },
+  },
+
+  Team: {
+    id: (root) => root.id,
+    name: (root) => root.name,
+    category: (root) => root.category,
+    members: (root) => root.members,
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
