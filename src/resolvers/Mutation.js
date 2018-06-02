fe56cac63ae2a86032f5ad8b1035f4b6f7fe4d0a
Cristiano Santos <cristianossd@gmail.com>
2018-06-02 16:56:05 -0300

    Edit and delete for Team and Event

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { APP_SECRET, getUserId } = require('../utils');
 
-/* User */
+// User
 const signup = async (root, args, context, info) => {
   const password = await bcrypt.hash(args.password, 10);
   const user = await context.db.mutation.createUser({
@@ -35,7 +35,7 @@ const login = async (root, args, context, info) => {
   };
 };
 
-/* Team */
+// Team
 const createTeam = async (root, args, context, info) => {
   return context.db.mutation.createTeam({
     data: {
@@ -46,7 +46,20 @@ const createTeam = async (root, args, context, info) => {
   }, info);
 };
 
-/* Event */
+const updateTeam = async (root, args, context, info) => {
+  return context.db.mutation.updateTeam({
+    where: { name: args.name },
+    data: args,
+  }, info);
+};
+
+const deleteTeam = async (root, args, context, info) => {
+  return context.db.mutation.deleteTeam({
+    where: { name: args.name },
+  }, info);
+};
+
+// Event
 const createEvent = async (root, args, context, info) => {
   const team = await context.db.query.team({
     where: { name: args.teamName },
@@ -64,9 +77,46 @@ const createEvent = async (root, args, context, info) => {
   }, info)
 };
 
+const updateEvent = async (root, args, context, info) => {
+  const team = await context.db.query.team({
+    where: { name: args.teamName },
+  }, `{ id }`);
+
+  if (!team) {
+    throw new Error('You are trying to use undefined team');
+  }
+
+  const eventId = args.id;
+  delete args.id;
+  delete args.teamName;
+
+  return context.db.mutation.updateEvent({
+    where: { id: eventId },
+    data: args,
+  }, info);
+};
+
+const deleteEvent = async (root, args, context, info) => {
+  const team = await context.db.query.team({
+    where: { name: args.teamName },
+  }, `{ id }`);
+
+  if (!team) {
+    throw new Error('You are trying to use undefined team');
+  }
+
+  return context.db.mutation.deleteEvent({
+    where: { id: args.id },
+  }, info);
+};
+
 module.exports = {
   signup,
   login,
   createTeam,
+  updateTeam,
+  deleteTeam,
   createEvent,
+  updateEvent,
+  deleteEvent,
 };
