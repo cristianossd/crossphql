b71e9a28c479837b804c8b27cc478f7184208990
Cristiano Santos <cristianossd@gmail.com>
2018-06-01 22:06:10 -0300

    Setup authentication

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const { Prisma } = require('prisma-binding');
 
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
+const AuthPayload = require('./resolvers/AuthPayload');
 
 const resolvers = {
   Query,
   Mutation,
+  AuthPayload,
 };
 
 const server = new GraphQLServer({
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,41 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET, getUserId } = require('../utils');
+
+/* User */
+const signup = async (root, args, context, info) => {
+  const password = await bcrypt.hash(args.password, 10);
+  const user = await context.db.mutation.createUser({
+    data: { ...args, password },
+  }, `{ id }`);
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+};
+
+const login = async (root, args, context, info) => {
+  const user = await context.db.query.user({ where: { email: args.email } }, `{ id password }`);
+  if (!user) {
+    throw new Error(`No such user found`);
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error(`Invalid password`);
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+};
+
+/* Team */
 const createTeam = async (root, args, context, info) => {
   return context.db.mutation.createTeam({
     data: {
@@ -8,6 +46,7 @@ const createTeam = async (root, args, context, info) => {
   }, info);
 };
 
+/* Event */
 const createEvent = async (root, args, context, info) => {
   const team = await context.db.query.team({
     where: { name: args.teamName },
@@ -26,6 +65,8 @@ const createEvent = async (root, args, context, info) => {
 };
 
 module.exports = {
+  signup,
+  login,
   createTeam,
   createEvent,
 };
